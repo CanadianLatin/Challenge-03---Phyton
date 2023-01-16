90343111ef9c0f691667102a54bef053db6921bd
CanadianLatin <data@marquezfamily.ca>
2023-01-15 19:35:29 -0500

    PyBank Complete! :-)

--- PyBank/main_bank.py
+++ PyBank/main_bank.py
@@ -6,6 +6,7 @@ import os
 import csv
 
 csvpath = os.path.join(".", "Resources", "budget_data.csv")
+textpath = os.path.join(".","Analysis","results.txt")
 
 # Lists to store the Data
 date = []
@@ -36,10 +37,10 @@ with open(csvpath) as csvfile:
 
        
 
-print("Financial Analysis")
-print("-----------------------------------")
-print("Total Months:", len(date))
-print("Total Revenue: $", sum(value))
+print('\n'+'\n'+'\n'+'\t'+'\t'+"Financial Analysis:")
+print( '\n'+"-----------------------------------------------------")
+print('\n'+"Total Months:"+'\t'+'\t'+'\t', len(date))
+print('\n'+"Total:"+'\t'+'\t'+'\t'+'\t'+"$ ", sum(value))
 
 # in the following for I will use value to get the minimum, maximum, average information
 for i in range(1,len(value)):
@@ -47,9 +48,21 @@ for i in range(1,len(value)):
     avg_difference = sum(difference)/len(difference)
     max_difference = max(difference)
     min_difference = min(difference)
-    max_difference_date = str(date[difference.index(max(difference))])
-    min_difference_date = str(date[difference.index(min(difference))])
+    max_difference_date = str(date[difference.index(max(difference))+1])
+    min_difference_date = str(date[difference.index(min(difference))+1])
 
-print("Avereage Revenue Change: $", round(avg_difference))
-print("Greatest Increase in Revenue:", max_difference_date,"($", max_difference,")")
-print("Greatest Decrease in Revenue:", min_difference_date,"($", min_difference,")")
+print('\n'+"Average Change:"+'\t'+'\t'+'\t'+"$ ", round(avg_difference,2))
+print('\n'+"Greatest Increase in Profits: "+'\t', max_difference_date,"($", max_difference,")")
+print('\n'+"Greatest Decrease in Profits: ", min_difference_date,"($", min_difference,")"+'\n'+'\n'+'\n')
+
+
+with open(textpath, 'w') as f:
+    f.write('\n'+'\n'+'\n'+'\t'+'\t'+"Financial Analysis:")
+    f.write( '\n'+"-----------------------------------------------------")
+    f.write('\n'+"Total Months:"+'\t'+'\t'+'\t'+str (len(date)))
+    f.write('\n'+"Total:"+'\t'+'\t'+'\t'+'\t'+"$ "+str(sum(value)))
+    f.write('\n'+"Average Change:"+'\t'+'\t'+'\t'+"$ "+ str(round(avg_difference,2)))
+    f.write('\n'+"Greatest Increase in Profits: "+'\t'+ str(max_difference_date)+'\t'+"($"+str(max_difference)+")")
+    f.write('\n'+"Greatest Decrease in Profits: "+'\t'+ str( min_difference_date)+'\t'+"($"+str(min_difference)+")")
+
+    
